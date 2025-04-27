07f0d6dc6f0b338be18d0cb81493707187e9810e
soyello <wodus9193@naver.com>
2025-04-27 19:19:05 +0900

    netlify 배포용 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  output: "standalone",
+  output: 'export',
 
   // topLevelAwait 경고 방지용
   webpack: (config, { isServer }) => {
@@ -13,7 +12,6 @@ const nextConfig: NextConfig = {
         asyncFunction: true,
       };
     }
-
     return config;
   },
 };
